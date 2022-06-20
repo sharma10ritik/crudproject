f949eef48431063f47bef9c411e03e837f0e167c
beatruefighter <104251748+beatruefighter@users.noreply.github.com>
2022-06-20 16:16:50 +0530

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,143 @@
+   var selectedRow=null
+function onFormSubmit(){
+    if (validate()){
+    var formData=readFormData();
+    if(selectedRow==null)
+    insertNewRecord(formData);
+    else
+    updateRecord(formData);
+    resetForm();
+}
+}
+function readFormData(){
+var formData={};
+formData["fullName"]= document.getElementById("fullname").value;
+formData["empcode"]= document.getElementById("empcode").value;
+formData["salary"]= document.getElementById("salary").value;
+formData["city"]= document.getElementById("city").value;
+return formData;
+}
+function insertNewRecord(data){
+    var table=document.getElementById("employeeList").getElementByTagName('tbody')[0];
+    var newRow=table.insertRow(table.length);
+    cell1=newRow.insertCell(0);
+    cell1.innerHTML=data.fullName; 
+    cell2=newRow.insertCell(1);
+    cell2.innerHTML=data.empcode; 
+    cell3=newRow.insertCell(2);
+    cell3.innerHTML=data.salary; 
+    cell4=newRow.insertCell(3);
+    cell4.innerHTML=data.city; 
+    cell4=newRow.insertCell(4);
+    cell4.innerHTML= '<a onClick-"onEdit(this)">Edit</a> 
+                     <a>Delete</a>';
+}
+function resetForm()
+{
+
+document.getElementById("fullname").value = "";
+    
+document.getElementById("empcode").value = "";
+    
+document.getElementById("salary").value = "";
+    
+document.getElementById("city").value = "";
+}
+function onEdit(td)
+{
+    selectedRow=td.parentElement.parentElement;
+    document.getElementById("fullname").value=selectedRow.cells[0].innerHTML;
+    document.getElementById("empcode").value=selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value=selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value=selectedRow.cells[3].innerHTML;
+
+
+}
+function updateRecord(formData)
+{
+    selectedRow.cells[0].innerHTML=formData.fullName;
+    selectedRow.cells[1].innerHTML=formData.empcode;
+    selectedRow.cells[2].innerHTML=formData.salary;
+    selectedRow.cells[3].innerHTML=formData.city;
+}
+function onDelete(td)
+{
+    if(confirm("are you sure to delete record?"){
+        row=td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+
+}
+function validate()
+{
+    isValid = true;
+    if(document.getElementById("fullname").value == "")
+    {
+        isValid=false;
+        document.getElementById("fullnamevalidationerror").classList.remove("hide");
+    }
+    else
+    {
+        isvalid=true;
+        if(!document.getElementById("fullnamevalidationerror").classList.contains("hide"))
+        document.getElementById("fullnamevalidationerror").classList.add("hide");
+    }
+return isValid;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
